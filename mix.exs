36352199703a30658ed4d90e4024bcd251fd07c7
jkorth <jake@modinfinity.com>
2019-05-27 20:38:38 -0500

    Add dialyxir dependency
    This adds the dialyxir dependency so dialyzer commands can be run. This
    will be used for static analysis.

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Star.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false}
     ]
   end
 
